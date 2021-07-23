592659a2ec88beeb490cf446ed8bcc09b39b8f13
Mohammad Noroozi <mm.74noroozi@gmail.com>
2021-07-24 02:02:43 +0430

    template category page

---
+++ pages/categories/_categories.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-card height="400" flat>
+    <v-layout justify-space-between align-center fill-height>
+      <v-card height="60" width="60" >  </v-card>
+      <v-card height="60" width="60" >  </v-card>
+    </v-layout>
+  </v-card>
+</template>
+
+<script>
+export default {
+  header: {
+    title: 'my website title',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'my website description',
+      },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+}
+</script>
