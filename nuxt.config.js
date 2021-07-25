d80d1f736a40c369361854e32e1bbf6dad3bc582
Mohammad Noroozi <mm.74noroozi@gmail.com>
2021-07-26 00:55:51 +0430

    feat: products

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,7 +46,9 @@ export default {
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
+  axios: {
+    baseURL: 'https://damirco.com'
+  },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
---
+++ pages/products/product-detail/_slug.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-card>{{description}}</v-card>
+</template>
+
+<script>
+export default {
+    data: ()=>{
+        page: {}
+    },
+    head: ()=> {
+      title: this.page.title,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'My custom description'
+        }
+      ]
+    },
+    async fetch({ $axios,params }) {
+      this.page = await $axios.get(`/products/api/products/?slug=${params.slug}`);
+    }
+}
+</script>
