67184be3f1c2474c302d61e81079bc517a096fbc
Mohammad Noroozi <mm.74noroozi@gmail.com>
2021-07-28 02:53:54 +0430

    fix: product fourth edit

---
+++ components/ProductRating.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="d-flex flex-column justify-center align-center">
+    <span>
+      <span class="font-weight-bold">{{ rating }}</span
+      ><span class="text-caption">/5</span>
+    </span>
+    <v-rating
+      v-model="rating"
+      color="yellow darken-3"
+      background-color="grey darken-1"
+      empty-icon="$ratingFull"
+      hover
+      size="20"
+      style="direction: ltr"
+    ></v-rating>
+    <span class="text-caption">امتیاز براساس نظر مشتریان</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    rating: 4.3,
+  }),
+}
+</script>
--- pages/products/product-detail/_slug.vue
+++ pages/products/product-detail/_slug.vue
@@ -1,15 +1,74 @@
 <template>
   <v-layout column>
     <v-breadcrumbs :items="breadcrumbs" divider="/" />
+    <v-card elevation="0" class="my-5 pa-2">
+      <v-row>
+        <v-col cols="12" md="6">
+          <v-layout justify-space-between align-center class="d-md-none">
+            <v-card
+              elevation="0"
+              class="primary--text"
+              v-text="page.title"
+            ></v-card>
+            <ProductRating />
+          </v-layout>
+          <v-carousel v-model="model" >
+            <v-carousel-item v-for="(item,i) in carousel_items" :key="i" :src="item.src" />
+          </v-carousel>
+        </v-col>
+        <v-col cols="12" md="6" class="pa-5 d-flex flex-column justify-space-between">
+          <v-layout justify-space-between align-center  class="flex-grow-0">
+            <v-card
+              elevation="0"
+              class="primary--text"
+              v-text="page.title"
+            ></v-card>
+            <ProductRating />
+          </v-layout>
+          <v-card
+            elevation="0"
+            text
+            class="my-5 py-5 flex-grow-1"
+            v-text="page.short_discription"
+          ></v-card>
+          <v-row class="flex-grow-0">
+            <v-col cols="12" md="9">
+              <div class="font-weight-bold">
+                آخرین بروزرسانی قیمت <span>{{ page.last_edited }}</span>
+              </div>
+              <div class="font-weight-bold">
+                قیمت:
+                <span class="text-h5 primary--text"
+                  >{{ page.price }} تومان</span
+                >
+              </div>
+            </v-col>
+            <v-col cols="12" md="3" class="d-flex flex-column justify-end">
+              <v-btn color="primary">استعلام قیمت</v-btn>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-card>
     <v-row>
       <v-col cols="12" md="8" class="pa-2">
-        <v-card elevation="0" class="pa-5 text-center font-weight-bold mb-1 ">توضیحات محصول</v-card>
-        <v-card elevation="0" v-html="page.description" class="px-2 py-5"></v-card>
+        <v-card elevation="0" class="pa-5 text-center font-weight-bold mb-1"
+          >توضیحات محصول</v-card
+        >
+        <v-card
+          elevation="0"
+          v-html="page.description"
+          class="px-2 py-5"
+        ></v-card>
       </v-col>
       <v-col cols="12" md="4" class="pa-2">
-        <v-card elevation="0" class="pa-5 text-center font-weight-bold mb-1 ">مشخصات محصول</v-card>
-        <v-card elevation="0" >
-          <v-card elevation="0" class="pa-5 text-center grey lighten-4">وارد نشده</v-card>
+        <v-card elevation="0" class="pa-5 text-center font-weight-bold mb-1"
+          >مشخصات محصول</v-card
+        >
+        <v-card elevation="0">
+          <v-card elevation="0" class="pa-5 text-center grey lighten-4"
+            >وارد نشده</v-card
+          >
         </v-card>
       </v-col>
     </v-row>
@@ -25,37 +84,50 @@ export default {
       )
     ).data
     if (data.count > 0) {
-      const result= data.results[0]
-      return { 
+      const result = data.results[0]
+      return {
         page: result,
         breadcrumbs: [
-        {
-          text: 'خانه',
-          disabled: false,
-          href: '/',
-        },
-        {
-          text: result.category[0].title,
-          disabled: false,
-          href: `/categories/${result.category[0].slug}`,
-        },
-        {
-          text: result.title,
-          disabled: false,
-          href: `/products/product-detail/${result.slug}`,
-        },
-      ],
+          {
+            text: 'خانه',
+            disabled: false,
+            href: '/',
+          },
+          {
+            text: result.category[0].title,
+            disabled: false,
+            href: `/categories/${result.category[0].slug}`,
+          },
+          {
+            text: result.title,
+            disabled: false,
+            href: `/products/product-detail/${result.slug}`,
+          },
+        ],
+        carousel_items: [
+          {
+            src: "https://test.damirco.com/images/documents/blobid1604672419153.jpg",
+            alt: result.image_alt,
+          },
+        ],
       }
     }
   },
   data() {
     return {
-      title:"ss",
+      title: 'ss',
       page: {
-        slug:"",
+        slug: '',
         category: [{}],
       },
-      breadcrumbs:[]
+      breadcrumbs: [],
+      colors: ['primary', 'secondary', 'yellow darken-2', 'red', 'orange'],
+      rating: 4.3,
+      carousel_items: [
+        {
+          src: '',
+        },
+      ],
     }
   },
   head() {
