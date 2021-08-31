8ea8756095a13cf2ecb4dd3fbfadd364e6daff79
Mohammad Noroozi <mm.74noroozi@gmail.com>
2021-08-31 07:22:18 +0430

    fix: change env name

--- services/EventService.js
+++ services/EventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: process.env.BROWSER_BASE_API_URL || 'http://127.0.0.1:8000',
+  baseURL: process.env.API_URL_BROWSER || 'http://127.0.0.1:8000',
   // baseURL:'http://damirco.com',
   withCredentials: false,
   headers: {
