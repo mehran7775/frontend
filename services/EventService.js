4d238a993fc82776988c0ff442ae4e3df85a3e0e
mohammad <mm.74noroozi@gmail.com>
2021-08-31 08:16:50 +0430

    fix: correction on relative url

--- services/EventService.js
+++ services/EventService.js
@@ -9,39 +9,39 @@ const apiClient = axios.create({
 
 export default {
   get_csrf_token() {
-    return apiClient.get('api/get-csrftoken')
+    return apiClient.get('/api/get-csrftoken')
   },
   async do_register(form) {
     const re = await this.get_csrf_token()
     const csrf = re.data.csrftoken
-    return apiClient.post('api/signup', form, {
+    return apiClient.post('/api/signup', form, {
       headers: {
         'X-CSRFToken': csrf
       }
     })
   },
   get_product(slug) {
-    return apiClient.get('api/products/' + slug)
+    return apiClient.get('/api/products/' + slug)
   },
 
   //-------UserPanel-----------//
 
   get_orders(payload) {
-    return apiClient.get('api/userpanel/orders', {
+    return apiClient.get('/api/userpanel/orders', {
       headers: {
         "Authorization": payload
       }
     })
   },
   get_inquiries(payload) {
-    return apiClient.get('api/userpanel/inquiries', {
+    return apiClient.get('/api/userpanel/inquiries', {
       headers: {
         "Authorization": payload
       }
     })
   },
   get_inquiry(payload) {
-    return apiClient.get(`api/userpanel/inquiries/${payload.id}`, {
+    return apiClient.get(`/api/userpanel/inquiries/${payload.id}`, {
       headers: {
         "Authorization": payload.token
       }
@@ -50,7 +50,7 @@ export default {
   async verify_order(payload) {
     const re = await this.get_csrf_token()
     const csrf = re.data.csrftoken
-    return apiClient.post('api/userpanel/orders_verify/' + payload.id, null, {
+    return apiClient.post('/api/userpanel/orders_verify/' + payload.id, null, {
       headers: {
         "Authorization": payload.token,
         'X-CSRFToken': csrf
@@ -60,7 +60,7 @@ export default {
   async create_product(payload) {
     const re = await this.get_csrf_token()
     const csrf = re.data.csrftoken
-    return apiClient.post('api/userpanel/products', payload.form, {
+    return apiClient.post('/api/userpanel/products', payload.form, {
       headers: {
         "Authorization": payload.token,
         "content-type": "multipart/form-data",
@@ -69,7 +69,7 @@ export default {
     })
   },
   get_products_supplier(payload) {
-    return apiClient.get('api/userpanel/products', {
+    return apiClient.get('/api/userpanel/products', {
       headers: {
         "Authorization": payload
       }
@@ -80,7 +80,7 @@ export default {
     const csrf = re.data.csrftoken
     let form = new FormData()
     form.append('_method', 'DELETE')
-    return apiClient.post('api/userpanel/products/' + payload.id, form, {
+    return apiClient.post('/api/userpanel/products/' + payload.id, form, {
       headers: {
         "Authorization": payload.token,
         'X-CSRFToken': csrf
@@ -90,7 +90,7 @@ export default {
   async complete_information(payload) {
     const re = await this.get_csrf_token()
     const csrf = re.data.csrftoken
-    return apiClient.post('api/userpanel/user/edit', payload.form, {
+    return apiClient.post('/api/userpanel/user/edit', payload.form, {
       headers: {
         "Authorization": payload.token,
         'X-CSRFToken': csrf
@@ -98,7 +98,7 @@ export default {
     })
   },
   get_product_edit(payload) {
-    return apiClient.get('api/userpanel/products/' + payload.id, {
+    return apiClient.get('/api/userpanel/products/' + payload.id, {
       headers: {
         "Authorization": payload.token
       }
@@ -107,7 +107,7 @@ export default {
   async edit_product(payload) {
     const re = await this.get_csrf_token()
     const csrf = re.data.csrftoken
-    return apiClient.post(`api/userpanel/products/${payload.id}`, payload.form, {
+    return apiClient.post(`/api/userpanel/products/${payload.id}`, payload.form, {
       headers: {
         "Authorization": payload.token,
         "content-type": "multipart/form-data",
@@ -120,7 +120,7 @@ export default {
     const csrf = re.data.csrftoken
     let form = new FormData()
     form.append('phone_number', JSON.stringify(payload))
-    return apiClient.post('api/get-phone/', form, {
+    return apiClient.post('/api/get-phone/', form, {
       headers: {
         'X-CSRFToken': csrf
       }
